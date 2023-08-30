1c271dc0feedaf2750a82e737710ec69b57f5013
Chandan Gowda Nandakumar - Contractor <cgowda@mobilemini.com>
2023-08-30 09:44:07 -0700

    8/30

--- index.js
+++ index.js
@@ -2,78 +2,75 @@ import axios from "axios";
 export const handler = async (event, context) => {
   const bearerToken = "4c3c29cd-017b-463e-9bf9-50da0e7e0030";
   let response;
-  //   try {
-  //     const tokenUrl = "https://api.ariba.com/v2/oauth/token";
-  //     const queryParams = {
-  //       grant_type: "openapi_2lo",
-  //     };
-  //     const headers = {
-  //       "apiKey": "stjCiIZILvIZwTWJUTWxzE5ykPCe8s3p",
-  //       "Content-Type": "application/json",
-  //     };
+    try {
+      const tokenUrl = "https://api.ariba.com/v2/oauth/token";
+      const queryParams = {
+        grant_type: "openapi_2lo",
+      };
+      const headers = {
+        "apiKey": "stjCiIZILvIZwTWJUTWxzE5ykPCe8s3p",
+        "Authorization": "Basic "+ process.env.BASE64_ENCODED_CLIENT_SERVER
+      };
 
-  //     const auth = {
-  //       username: "stjCiIZILvIZwTWJUTWxzE5ykPCe8s3p",
-  //       password: "KGlVTA8U8AjybmdhAAKcqMoWqqV6DcF6",
-  //     };
+      const res = await axios.get(tokenUrl, {
+        params: queryParams,
+        headers: headers,
+      });
 
-  //     const res = await axios.get(tokenUrl, {
-  //       params: queryParams,
-  //       headers: headers,
-  //       auth: auth,
-  //     });
+      console.log(JSON.stringify(res));
 
-  //     console.log(res);
+      response = {
+        statusCode: 200,
+        body: res,
+      };
+    } catch (error) {
+      console.log(error);
 
-  //     response = {
-  //       statusCode: 200,
-  //       body: res,
-  //     };
-  //   } catch (error) {
-  //     console.log(error);
+      response = {
+        statusCode: 500,
+        body: "error",
+      };
 
-  //     response = {
-  //       statusCode: 500,
-  //       body: "error",
-  //     };
+      throw error;
+    }
 
-  //     throw error;
-  //   }
 
-  try {
-    const tokenUrl =
-      "https://openapi.ariba.com/api/approval/v2/prod/pendingApprovables";
-    const queryParams = {
-      realm: "WillScotMobileMini-1-T",
-    };
-    const headers = {
-      apiKey: "stjCiIZILvIZwTWJUTWxzE5ykPCe8s3p",
-      accept: "application/json",
-      Authorization: "Bearer " + bearerToken,
-    };
-
-    const res = await axios.get(tokenUrl, {
-      params: queryParams,
-      headers: headers,
-    });
-
-    console.log(res);
-
-    response = {
-      statusCode: 200,
-      body: res,
-    };
-  } catch (error) {
-    console.log(error);
-
-    response = {
-      statusCode: 500,
-      body: "error",
-    };
-
-    throw error;
-  }
-
-  console.log(response);
+    // try {
+    //   const tokenUrl =
+    //     "https://openapi.ariba.com/api/approval/v2/prod/pendingApprovables";
+    //   const queryParams = {
+    //     realm: "WillScotMobileMini-1-T",
+    //   };
+    //   const headers = {
+    //     apiKey: "stjCiIZILvIZwTWJUTWxzE5ykPCe8s3p",
+    //     accept: "application/json",
+    //     Authorization: "Bearer " + bearerToken,
+    //   };
+  
+    //   const res = await axios.get(tokenUrl, {
+    //     params: queryParams,
+    //     headers: headers,
+    //   });
+  
+    //   console.log(res);
+  
+    //   response = {
+    //     statusCode: 200,
+    //     body: JSON.stringify(res),
+    //   };
+    // } catch (error) {
+    //   console.log(error);
+  
+    //   response = {
+    //     statusCode: 500,
+    //     body: "error",
+    //   };
+  
+    //   throw error;
+    // }
+  
+  
+  console.log('response', response);
   return response;
 };
+
